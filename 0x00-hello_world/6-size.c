6af16f5e52ec70320542f29772092212cd52c25c
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-18 23:52:10 +0000

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	puts("Size of a char: %lu bytes(s)", sizeof(char));
-	puts("Size of an int: %lu bytes(s)", sizeof(int));
-	puts("Size of a long int: %lu bytes(s)", sizeof(long int));
-	puts("Size of a long long int: %lu bytes(s)", sizeof(long long int));
-	puts("Size of a float: %lu bytes(s)", sizeof(float));
+	printf("Size of a char: %lu bytes(s)", sizeof(char));
+	printf("Size of an int: %lu bytes(s)", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)", sizeof(float));
 	return (0);
 }
