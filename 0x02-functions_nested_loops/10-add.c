e4e22c29605d613266493856f18eacd9f63043a0
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-19 21:49:43 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @x: integer
+ * @y: integer
+ * Return: result
+ */
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+	return (result);
+}
