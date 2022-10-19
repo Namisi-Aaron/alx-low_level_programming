c75dcbcfa7a84c6068e6878698c851108be89dd2
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-19 22:51:24 +0000

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints natural numbers to 98
+ * @n: starting number
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d", n);
+	}
+}
+
