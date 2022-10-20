42c2a330e0378d030ad09348d2df066cae009291
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-20 00:12:40 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers of base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+}
