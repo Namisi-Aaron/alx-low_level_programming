1ae8643eccbabbaf94344a086116c428933816fb
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-20 01:56:46 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations
+ * of single digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		putchar(x + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
