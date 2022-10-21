96ea564a5d4def2f895b307627c04bb1f8fc896e
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-21 12:51:07 +0000

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+	_putchar(x + '0');
+	return (0);
+}
