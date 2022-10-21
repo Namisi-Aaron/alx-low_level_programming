c20d237723ab107c0ebc17d038415d88853aceb8
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-21 22:17:34 +0000

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (x = 1 ; x <= size ; x++)
+		{
+			for (y = size - x ; y > 0 ; y--)
+			{
+				_putchar(' ');
+			}
+			for (y = 0 ; y < x ; y++)
+			{
+				_putchar('#');
+			}
+			if (x == size)
+			{
+				continue;
+			}
+		}
+	}
+}
