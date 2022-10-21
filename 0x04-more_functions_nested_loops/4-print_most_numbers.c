a5d578a089d32314c3d6e631b0cdaf6e941a38c1
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-21 19:45:59 +0000

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - prints 0 - 9
- * without 2 and 4
+ * print_most_numbers - prints 0 - 9 without 2 and 4
  * Return: always 0
  */
 void print_most_numbers(void)
