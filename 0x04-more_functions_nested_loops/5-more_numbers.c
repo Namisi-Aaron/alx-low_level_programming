ac1cb0c3528006b1078895a335df4d0db2cbbb68
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-21 20:45:54 +0000

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0 times 0 to 14
+ * Retur: Always 0
+ */
+void more_numbers(void)
+{
+	char x;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		char y;
+
+		for (y = 0 ; y <= 14 ; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
