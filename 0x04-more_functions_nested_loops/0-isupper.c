5a3e8b8cde7072a0cd3508b2cf71ceebe4b87334
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-21 11:54:00 +0000

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: character
+ * Return: 1 for uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
