01b5787ffcd0baa1892b3b854fb4905d65372bf6
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-24 08:45:08 +0000

    Print number

---
+++ 0x03-debugging/101-print_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ * Return: always 0
+ */
+void print_number(int n)
+{
+	_putchar(n + '0');
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,5 +5,6 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_number(int n);
 
 #endif /* MAIN_H */
