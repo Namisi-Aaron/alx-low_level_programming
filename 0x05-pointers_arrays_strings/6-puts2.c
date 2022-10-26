ef48331c2b3e50bd9bdb45116202eecc2f2f47f0
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-26 21:34:09 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -18,24 +18,4 @@ void puts2(char *str)
 			continue;
 	}
 	_putchar('\n');
-}#include "main.h"
-/**
- * puts2 - prints one character out of two
- * @str: input
- * Return: print
- */
-void puts2(char *str)
-{
-	int i;
-
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if ((i % 2) == 0)
-		{
-			_putchar(str[i]);
-		}
-		else
-			continue;
-	}
-	_putchar('\n');
 }
