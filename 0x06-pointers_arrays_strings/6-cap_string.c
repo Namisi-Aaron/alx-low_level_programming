92009dad1b0ada7a08d458dd9a8d6f4e488b1a50
Aaron Namisi <aaronnamisi@gmail.com>
2022-10-30 00:26:01 +0000

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to be capitalized
+ * Return: String pointer
+ */
+char *cap_string(char *str)
+{
+	int v;
+
+	while (str[v])
+	{
+		while (!(str[v] >= 'a' && str[v] <= 'z'))
+		{
+			v++;
+		}
+		if (str[v - 1] == ' ' ||
+			str[v - 1] == '\t' ||
+			str[v - 1] == '\n' ||
+			str[v - 1] == '.' ||
+			str[v - 1] == ',' ||
+			str[v - 1] == ';' ||
+			str[v - 1] == '!' ||
+			str[v - 1] == '?' ||
+			str[v - 1] == ')' ||
+			str[v - 1] == '(' ||
+			str[v - 1] == '}' ||
+			str[v - 1] == '{' ||
+			str[v - 1] == '"' ||
+			v == 0)
+		{
+			str[v] -= 32;
+		}
+		v++;
+	}
+	return (str);
+}
