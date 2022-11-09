ea2c17ea61890781cc7a4e9588a6c052d106349a
Aaron Namisi <aaronnamisi@gmail.com>
2022-11-09 22:16:51 +0000

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of characters
+ * @size: array size
+ * @c: initialization character
+ * Return: character pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	a = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (NULL);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		a[i] = c;
+
+	free(a);
+	return (a);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+Malloc and free
+malloc() - used to allocate block of memory
+free() - Deallocates block of memory
