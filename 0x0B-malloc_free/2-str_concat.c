694ff35c50f9a62f6603e7fbfead2de91930052c
Aaron Namisi <aaronnamisi@gmail.com>
2022-11-09 23:39:18 +0000

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	j = 0;
 
-	while (s1[i] != NULL)
+	while (s1[i] != '\0')
 		i++;
-	while (s2[j] != NULL)
+	while (s2[j] != '\0')
 		j++;
 	conc = malloc(sizeof(char) * (i + j + 1));
 	if (conc == NULL)
@@ -28,12 +28,12 @@ char *str_concat(char *s1, char *s2)
 
 	i = 0;
 	j = 0;
-	while (s1 != '\0')
+	while (s1[i] != '\0')
 	{
 		conc[i] = s1[i];
 		i++;
 	}
-	while (s2 != '\0')
+	while (s2[j] != '\0')
 	{
 		conc[i] = s2[j];
 		i++;
