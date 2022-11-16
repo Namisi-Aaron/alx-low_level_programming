7104e3de35ae3911c6a5559c64d21f5165de198f
Aaron Namisi <aaronnamisi@gmail.com>
2022-11-16 01:17:06 +0000

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,20 +8,14 @@
  * @owner: owner's name
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
-/**
- * dog_t - typedef keyword for new struct name
- */
-
-typedef struct dog dog_t;
 
 #endif
