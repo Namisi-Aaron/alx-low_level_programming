af1115d1627155fd872a92f5f03372a7bf942d5f
Aaron Namisi <aaronnamisi@gmail.com>
2022-11-17 02:43:36 +0000

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees memory alocated for a struct dog
+ * @d: struct to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
