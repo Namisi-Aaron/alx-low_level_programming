45db00f46ae074e98b721d46a0a9187a70b143ab
Aaron Namisi <aaronnamisi@gmail.com>
2022-11-17 22:50:20 +0000

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,13 @@
 #include "function_pointers.h"
 /**
  * print_name - prints a name
+ * @name: name to be printed
+ * @f: pointer to function the prints name
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+		return;
 	f(name);
 }
