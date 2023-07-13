72b5a7ca8406e82ffecdda82fc593ed74e5ccc7b
Namisi-Aaron <aaronnamisi@gmail.com>
2023-07-13 12:18:01 +0300

    hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main()
+{
+	const char *string = "\"Programming is like building a multilingual puzzle";
+
+	puts(string);
+	return 0;
+}
