01f8f16739579abff174e68a921724aa9c309e7c
Namisi-Aaron <aaronnamisi@gmail.com>
2023-07-14 11:00:36 +0300

    prints alphabet in lover and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0 on success
+ */
+int main(void)
+{
+	unsigned char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
