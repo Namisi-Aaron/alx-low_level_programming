b93599bbe0bef1abf924efa026c261f6c94b49bf
Namisi-Aaron <aaronnamisi@gmail.com>
2023-07-14 15:40:36 +0300

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,14 +5,14 @@
  */
 int main(void)
 {
-	      unsigned char alpha;
+	unsigned char alpha;
 
-	      for (alpha = 'a'; alpha <= 'z'; alpha++)
-	      {
-		      if (alpha == 'e' || alpha == 'q')
-			      continue;
-		      putchar(alpha);
-	      }
-	      putchar('\n');
-	      return (0);
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha == 'e' || alpha == 'q')
+			continue;
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
 }
