24d8627176691c7fb3cd6fc84c9d1df9f77c64a8
Namisi-Aaron <aaronnamisi@gmail.com>
2023-08-01 10:05:15 +0300

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strpbrk - searches a string for any of a set of bytes
  * @s: string to be scanned
@@ -19,5 +20,5 @@ char *_strpbrk(char *s, char *accept)
 		}
 		i++;
 	}
-	return ('0');
+	return (NULL);
 }
