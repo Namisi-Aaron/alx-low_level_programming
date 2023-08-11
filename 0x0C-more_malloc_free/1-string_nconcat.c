5aaf788372e3fc4484810c70f3a3ada4a9c318fe
Namisi-Aaron <aaronnamisi@gmail.com>
2023-08-11 09:08:49 +0300

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			i++;
 		}
 		ptr[i] = '\0';
-		return (ptr);
 	}
 	else if (n < strlen(s2))
 	{
@@ -51,6 +50,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			j++;
 		}
 		ptr[i] = '\0';
-		return (ptr);
 	}
+	return (ptr);
 }
