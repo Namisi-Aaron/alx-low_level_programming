ce92764e21cd69b770987ef2424c7412fc057988
Namisi-Aaron <aaronnamisi@gmail.com>
2023-08-14 14:16:23 +0300

    header file modified

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * dog - the types name
+ * struct dog - the types name
  * @name: the name of the struct dog
  * @age: the dog's age
  * @owner: the owner's name
